60e9f0fc7b16d172c1f1cf0e6fb1706fb9b3bf40
prathampl <83875080+prathampl@users.noreply.github.com>
2022-07-04 12:04:00 +0530

    Update 12.scala

--- 12.scala
+++ 12.scala
@@ -1,30 +1,29 @@
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd._
-object tweetmining {
+
+import scala.Ordering
+
+
+object tweet {
+
+
   val conf = new SparkConf()
     .setAppName("User mining")
     .setMaster("local[*]")
 
   val sc = new SparkContext(conf)
 
-  var pathToFile = ""
+
+  var pathToFile = "/home/student/navya/rt.json"
 
   def main(args: Array[String]) {
-    if (args.length != 1) {
-      println()
-      println("Dude, I need exactly one argument.")
-      println("But you have given me " + args.length +".")
-      println("The argument should be path to json file containing a bunch of tweets. esired.")
-      System.exit(1)
-    }
 
-    pathToFile = args(0)
     val tweets =
       sc.textFile(pathToFile).mapPartitions(TweetUtils.parseFromJson(_))
-    val tweetsByUser = tweets.map(x => (x.user, x)).groupByKey()
-     }
-    val numTweetsByUser = tweetsByUser.map(x => (x._1, x._2.size))
-    val sortedUsersByNumTweets = numTweetsByUser.sortBy(_._2, ascending=false)
+    val tweets_user = tweets.map(x=> (x.user, x)).groupByKey()
+    tweets.take(0).foreach{case (tweet) => println(tweet.user)}
+    val num = tweets_user.map(x => (x._1, x._2.size))
+    val sortedUsersByNumTweets = num.sortBy(_._2, ascending = false)
     sortedUsersByNumTweets.take(10).foreach(println)
 
   }
@@ -33,20 +32,19 @@ object tweetmining {
 import com.google.gson._
 
 object TweetUtils {
-	case class Tweet (
-		id : String,
-		user : String,
-		userName : String,
-		text : String,
-		place : String,
-		country : String,
-		lang : String
-		)
+  case class Tweet (
+                     id : String,
+                     user : String,
+                     userName : String,
+                     text : String,
+                     place : String,
+                     country : String,
+                     lang : String
+                   )
 
 
-	def parseFromJson(lines:Iterator[String]):Iterator[Tweet] = {
-		val gson = new Gson
-		lines.map(line => gson.fromJson(line, classOf[Tweet]))
-	}
+  def parseFromJson(lines:Iterator[String]):Iterator[Tweet] = {
+    val gson = new Gson
+    lines.map(line => gson.fromJson(line, classOf[Tweet]))
+  }
 }
-
